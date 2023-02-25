0e17bea5c8141ea3741995ad1ac2603fe24747bc
sordidvibes <125910301+sordidvibes@users.noreply.github.com>
2023-02-25 08:32:37 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sordid Vibes - The Band
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
